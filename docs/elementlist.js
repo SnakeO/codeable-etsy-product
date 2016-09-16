143b7abab5dd5dd2d3ca62dd3a8b2c65eeb5f642
Jacob Chapa <jake@otleylogic.com>
2016-09-16 00:42:56 -0500

    added docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","codeable_etsy_product\\admin\\EtsyProductAdmin"],["c","codeable_etsy_product\\etsy\\EtsyAPI"],["c","codeable_etsy_product\\frontend\\EtsyProduct"],["c","Composer\\Autoload\\ClassLoader"],["c","Composer\\Autoload\\ComposerStaticInitc265a4601488f8b5be1bd5ff372473b4"],["c","ComposerAutoloaderInitc265a4601488f8b5be1bd5ff372473b4"],["f","includeFile()"],["c","WC_Product_Etsy"]];
